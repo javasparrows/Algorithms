b67f1b14ed9e3950da33c904ea028dd68a4bce00
kashi1mochi <kashiwada@nablas.com>
2020-04-17 23:51:09 +0900

    fixed

--- AtCoder/arc061_a.py
+++ AtCoder/arc061_a.py
@@ -4,10 +4,12 @@ ans = 0
 
 for i in range(2**(n-1)):
     f = s[0]
+    
     for j in range(n-1):
         if i>>j & 1:
             f += '+'
         f += s[j+1]
+        
     ans += eval(f)
     
 print(ans)
