28d895825b3c453a4051928fd89fc24d8a3f8900
kashi1mochi <kashiwada@nablas.com>
2020-04-19 21:00:27 +0900

    fixed bug

--- AtCoder/abc147_c.py
+++ AtCoder/abc147_c.py
@@ -8,16 +8,16 @@ As = [[] for _ in range(N)]
 for i in range(N):
     n = int(input())
     for j in range(n):
-        person, state = [int(k) for k in input().split()]
+        person, state = map(int, input().split())
         As[i].append([person-1, state])
 
 honest = 0
-for i in range(2**N):
+for i in range(1, 2**N):
     flag = 0
     for j in range(N):  # 人一人ごとにループを回す
-        if (i >> j) & 1:   # 人jは正直と仮定
+        if (i>>j) & 1 == 1:   # 人jは正直と仮定
             for x, y in As[j]:
-                if (j >> x) & 1 != y:  # 人jは正直だが矛盾を発見
+                if (i>>x) & 1 != y:  # 人jは正直だが矛盾を発見
                     flag = 1
                     break
     if flag == 0:
