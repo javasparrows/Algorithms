868c57ad4e977940d43620506f2c99c83274f66f
Yuki Kashiwada <kashiwada@nablas.com>
2020-04-22 23:32:01 +0900

    Develop (#1)
    * modified
    
    * modified

---
+++ AtCoder/abc054_c_v1.py
@@ -0,0 +1,24 @@
+# 積を用いる方法
+
+import itertools
+
+N, M = map(int, input().split())
+adj_matrix = [[0]*N for _ in range(N)]
+
+# 隣接行列を作る
+for i in range(M):
+    a, b = map(int, input().split())
+    adj_matrix[a-1][b-1] = 1
+    adj_matrix[b-1][a-1] = 1
+
+cnt = 0
+
+for each in itertools.permutations(range(N)):
+    if each[0] != 0:  # 始点は1でないといけないから
+        break
+    factor = 1
+    for i in range(N-1):
+        factor *= adj_matrix[each[i]][each[i+1]]
+    cnt += factor
+
+print(cnt)
