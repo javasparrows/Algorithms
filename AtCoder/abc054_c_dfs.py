85c0aa06f07f210fcf813db029dd9a44d8fe4150
Yuki Kashiwada <kashiwada@nablas.com>
2020-04-24 13:47:58 +0900

    Develop (#2)
    * fixed bug
    
    * deleted unnecessary file

--- AtCoder/abc054_c_dfs.py
+++ AtCoder/abc054_c_dfs.py
@@ -4,7 +4,7 @@ N, M = map(int, input().split())
 adj_matrix = [[0]*N for _ in range(N)]
 
 # 隣接行列を作る
-for i in range(N):
+for i in range(M):
     a, b = map(int, input().split())
     adj_matrix[a-1][b-1] = 1
     adj_matrix[b-1][a-1] = 1
