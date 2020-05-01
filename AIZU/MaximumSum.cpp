43956dcbf9bfb0453005cfff43f0c63a0e581ae3
kashi1mochi <kashiwada@nablas.com>
2020-05-02 00:40:39 +0900

    modified

--- AIZU/MaximumSum.cpp
+++ AIZU/MaximumSum.cpp
@@ -3,27 +3,35 @@
 using namespace std;
 typedef long long ll;
 #define REP(i,n) for(ll i=0;i<(ll)(n);i++)
-#define FOR(i,a,b) for(ll i=a;i<=(ll)(b);i++)
-
-int N, k, i1, i2;
-ll a[100010];
-
+ 
 int main() {
-    while (cin >> N >> k) {
-        if (N == 0) break;
-        REP(i,N) cin >> a[i];
-
-        ll S[100010];
-        S[0] = 0;
-        REP(i,N) {
-            S[i+1] = S[i] + a[i];
-        }
-
-        int ans = 0;
-        REP(i,N+1-k) {
-            if (S[i+k] - S[i] > ans) ans = S[i+k] - S[i];
-        }
-
-        cout << ans << endl;
+    // エラトステネスのふるい
+    int MAX = 101010;
+    vector<int> is_prime(MAX, 1);
+    is_prime[0] = 0, is_prime[1] = 0;
+    for (int i = 2; i<MAX; i++) {
+        if (!is_prime[i]) continue;
+        for (int j = i*2; j < MAX; j += i) is_prime[j] = 0;
+    }
+    
+    // 2017likeかどうか
+    vector<int> a(MAX, 0);
+    for (int i = 0; i < MAX; i++) {
+        if (i%2 == 0) continue;
+        if (is_prime[i] && is_prime[(i+1)/2]) a[i] = 1;
+    }
+    
+    // 累積和
+    vector<int> s(MAX, 0);
+    for (int i = 0; i < MAX; i++) s[i+1] = s[i] + a[i];
+    
+    ll Q;
+    cin >> Q;
+    REP(i,Q) {
+        int l, r;
+        cin >> l >> r;
+        r++;
+        
+        cout << s[r] - s[l] << endl;
     }
 }
