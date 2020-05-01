545b946d9b13182cde172428a004ca26ef342d43
kashi1mochi <kashiwada@nablas.com>
2020-05-01 23:42:26 +0900

    added new solution

---
+++ AIZU/MaximumSum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for(ll i=0;i<(ll)(n);i++)
+#define FOR(i,a,b) for(ll i=a;i<=(ll)(b);i++)
+
+int N, k;
+
+
+int main() {
+    cin >> N >> k;
+    REP(i,N) cin >> a
+}
